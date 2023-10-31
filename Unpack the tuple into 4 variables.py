f05503869a1e483ef6dd61ca337067150d707757
vivek-repo01 <147979512+vivek-repo01@users.noreply.github.com>
2023-10-31 23:48:16 +0530

    Add files via upload

---
+++ Unpack the tuple into 4 variables.py
@@ -0,0 +1,13 @@
+#Unpack the tuple into 4 variables
+#Write a program to unpack the following tuple into four variables and display each variable.
+
+tuple1 = (10, 20, 30, 40)
+
+tuple1 = (10, 20, 30, 40)
+
+a, b, c, d = tuple1
+print(a)
+print(b)
+print(c)
+print(d)
+
