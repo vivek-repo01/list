e00e2440c69d6e963c993b8d05aabb07a8d6bd15
vivek-repo01 <147979512+vivek-repo01@users.noreply.github.com>
2023-10-31 23:43:26 +0530

    Add files via upload

---
+++ access value from tuple.py
@@ -0,0 +1,15 @@
+#Access value 20 from the tuple
+
+#The given tuple is a nested tuple. write a Python program to print the value 20.
+
+tuple1 = ("Orange", [10, 20, 30], (5, 15, 25))
+tuple1[1][1]
+
+#second way
+
+# understand indexing
+# tuple1[0] = 'Orange'
+# tuple1[1] = [10, 20, 30]
+# list1[1][1] = 20
+
+print(tuple1[1][1])
---
+++ reverse tuple.py
@@ -0,0 +1,9 @@
+#Reverse a tuple
+tuple1 = (10, 20, 30, 40, 50)
+
+tuple1[::-1]
+
+# Second way
+tuple1 = (10, 20, 30, 40, 50)
+tuple1 = tuple1[::-1]
+print(tuple1)
