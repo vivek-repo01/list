da7b3b3366845d16ef1b5e2305f2568b0b5b2171
vivek-repo01 <vivekdikshit@yahoo.com>
2024-04-10 22:28:57 +0530

    Added changes in swap file

--- swap two tuples in Python.py
+++ swap two tuples in Python.py
@@ -1,13 +1,13 @@
 #Swap two tuples in Python
 
-tuple11 = (11, 22)
-tuple22 = (99, 88)
+tuple1 = (11, 22)
+tuple2 = (99, 88)
 
-tuple11 = (11, 22)
-tuple22 = (99, 88)
+tuple1 = (11, 22)
+tuple2 = (99, 88)
 tuple1, tuple2 = tuple2, tuple1
-print(tuple22)
-print(tuple11)
+print(tuple2)
+print(tuple1)
 
 
 
